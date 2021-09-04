ea10ea6ce5027ed9471b97247b53d611d330b3af
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-09-05 06:07:04 +0700

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+const {resolve} = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb-typescript'
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  rules: {
+    // import related
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    // react
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-max-props-per-line': [1, { maximum: 1, when: 'always' }],
+    // code style
+    'max-len': ['warn', {
+      code: 150,
+      // will ignore everything inside html attrs
+      ignorePattern: '[\\w-]+="([^"]*)"',
+    }],
+    'no-underscore-dangle': 'off',
+    'func-names': 'off',
+  },
+}
---
+++ pages/hello.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next'
+import { Client } from '@notionhq/client'
+import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+
+const Hello: NextPage = (result) => {
+  useEffect(() => {
+    console.log(result)
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <span className="font-bold text-2xl bg-yellow-400">Hello mamen</span>
+    </div>
+  )
+}
+
+// export async function tryPull() {
+//   const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+//   const databaseId = "c2c863920391422384f88f2f844787ad"
+//   const response = await notion.databases.query({
+//     database_id: databaseId,
+//   })
+//   // console.log(notion)
+//   console.log(response)
+// }
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const databaseId = "c2c863920391422384f88f2f844787ad"
+  const res = await notion.databases.query({
+    database_id: databaseId,
+  })
+
+  console.log(res)
+  return {
+    props: ({ res })
+  }
+}
+
+// tryPull()
+
+export default Hello
