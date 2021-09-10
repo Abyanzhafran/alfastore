3f9d64bb223efe4b351f1d7a8c89235b174dc975
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-09-10 22:07:28 +0700

    feat(ui): :lipstick: update ui detail below - add function search, menu - update footer - update navbar - add assets

--- components/BaseFooter.tsx
+++ components/BaseFooter.tsx
@@ -1,31 +1,44 @@
-import { MenuAlt1Icon } from '@heroicons/react/outline';
-
 const BaseFooter = () => {
   return (
     <footer>
       <div className="flex w-full bg-gray-900 items-center justify-center">
         <div className="flex flex-col w-80 mx-4 my-8 justify-start gap-10">
           <span className="font-bold text-lg text-white leading-7 border-b-2">Alfastore</span>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
             <span className="text-white text-xs leading-4 font-bold mt-6 mb-4">Navigasi</span>
             <ul className="list-none text-white flex flex-col gap-4">
-              <li className="text-base leading-6 font-semibold">Beranda</li>
-              <li className="text-base leading-6 font-semibold">Tentang</li>
-              <li className="text-base leading-6 font-semibold">Katalog</li>
+              <li className="text-base leading-6 font-semibold cursor-pointer">Beranda</li>
+              <li className="text-base leading-6 font-semibold cursor-pointer">Tentang</li>
+              <li className="text-base leading-6 font-semibold cursor-pointer">Katalog</li>
             </ul>
           </div>
           <div className="flex flex-col gap-4">
             <span className="text-xs text-white leading-4 font-bold" >Media Sosial</span>
             <ul className="list-none text-white flex flex-col gap-4">
-              <li className="text-base leading-6 font-semibold">ACM</li>
-              <li className="text-base leading-6 font-semibold">ACM</li>
-              <li className="text-base leading-6 font-semibold">+62 8312 XXXX</li>
+              <li className="cursor-pointer flex items-center">
+                <div className="mr-1.5">
+                  <img src="/instagram.png" width="20" height="20" />
+                </div>
+                <span className="text-base leading-6 font-semibold">ACM</span>
+              </li>
+              <li className="cursor-pointer flex items-center">
+                <div className="mr-1.5">
+                  <img src="/fb.png" width="20" height="20" />
+                </div>
+                <span className="text-base leading-6 font-semibold">ACM</span>
+              </li>
+              <li className="cursor-pointer flex items-center">
+                <div className="mr-1.5">
+                  <img src="/whatsapp.png" width="20" height="20" />
+                </div>
+                <span className="text-base leading-6 font-semibold">+62 8312 XXXX</span>
+              </li>
             </ul>
           </div>
           <div className="flex flex-col">
-            <span className="text-white text-base leading-6 font-semibold mt-4" >Alamat</span>
+            <span className="text-white text-xs leading-4 font-bold mt-4" >Alamat</span>
             <span className="text-white text-base leading-6 font-semibold mt-4 mb-6">Jl . jalan, No. 45, Kartasura, Jawa Selatan</span>
-            <hr className="text-white mb-8 h-px" />
+            <hr className="text-white mb-2 h-px" />
             <span className="text-white text-xs leading-4 font-normal">© 2021 Alfastore (CV. Alfa Cipta Mukti). All rights reserved.</span>
           </div>
         </div>
--- components/BaseNavbar.tsx
+++ components/BaseNavbar.tsx
@@ -1,28 +1,48 @@
-import { MenuAlt1Icon, SearchIcon } from '@heroicons/react/outline';
+import { MenuAlt1Icon, SearchIcon, XIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
 
 const BaseNavbar = () => {
-  return (
-    <nav className="fixed w-full flex flex-col bg-white border-b border-gray-100 shadow-sm gap-4">
-      <div className="flex p-4 justify-between items-center">
-        {/* left nav */}
-        <div className="flex p-1">
-          <button className="">
-            <MenuAlt1Icon className="w-6 h-6 text-gray-900" />
-          </button>
-        </div>
+  const [isOpen, setOpen] = useState(false)
+  const [isSearch, setSearch] = useState(false)
+  const [isClose, setClose] = useState(false)
 
-        {/* brand */}
-        <div>
-          <div className="px-2 py-1">
-            <span className="font-bold text-lg text-gray-900 leading-7">Alfastore</span>
+  return (
+    <nav className="fixed w-full flex flex-col bg-white border-b-4 border-gray-100 shadow-sm gap-4">
+      <div className="flex flex-col">
+        <div className="flex p-4 justify-between items-center">
+          {/* left nav */}
+          <div className="flex p-1">
+            <button className={`${isSearch ? "hidden" : "block"} outline-none`} onClick={() => setOpen(!isOpen)}>
+              <MenuAlt1Icon className="w-6 h-6 text-gray-900" />
+            </button>
+            <button className={`${isSearch ? "block" : "hidden"} outline-none`} onClick={() => setClose(!isClose)}>
+              <XIcon className="w-6 h-6 text-gray-900" />
+            </button>
+          </div>
+
+          {/* brand */}
+          <div>
+            <div className="px-2 py-1">
+              <span className={`${isSearch ? "hidden" : "block"} font-bold text-lg text-gray-900 leading-7`}>Alfastore</span>
+              <input className={`${isSearch ? "block" : "hidden"} border-b-2 h-7 rounded focus:outline-none text-lg`} type="input" placeholder="Search..." />
+            </div>
+          </div>
+
+          {/* right nav */}
+          <div className="flex p-1">
+            <button className="outline-none" onClick={() => setSearch(!isSearch)}>
+              <SearchIcon className="w-6 h-6 text-gray-500" />
+            </button>
           </div>
         </div>
-
-        {/* right nav */}
-        <div className="flex p-1">
-          <button className="">
-            <SearchIcon className="w-6 h-6 text-gray-500" />
-          </button>
+        <div className={`w-full flex justify-center h-screen ${isOpen ? "block" : "hidden"}`}>
+          <ul className="flex flex-col gap-8 uppercase text-lg font-semibold items-center justify-center text-gray-900">
+            <li className="cursor-pointer active:text-gray-500">Beranda</li>
+            <li className="cursor-pointer active:text-gray-500">Tentang</li>
+            <li className="cursor-pointer active:text-gray-500">Produk</li>
+            <li className="cursor-pointer active:text-gray-500">Bantuan</li>
+            <nav className="font-bold normal-case text-lg text-gray-900 leading-7 fixed bottom-5">Alfastore</nav>
+          </ul>
         </div>
       </div>
     </nav>
