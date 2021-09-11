36dac622514893225d43195686064865e766d5a9
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-09-11 16:43:08 +0700

    feat: del homepage update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import type { NextPage } from 'next';
-import MainLayout from 'components/MainLayout';
+import type { NextPage } from 'next'
+import MainLayout from 'components/MainLayout'
 
-const homepage: NextPage = () => (
-  <MainLayout className="w-full max-w-sm mx-auto border-l border-r">
-    <div className="flex items-center justify-center h-screen">
-      <span className="font-bold text-xl">this is Homepage boyy</span>
-    </div>
-  </MainLayout>
-);
+const homepage: NextPage = () => {
+  return (
+    <MainLayout className="w-full">
+      <div className="relative h-screen flex items-end justify-start">
+        <img src="/alfa.png" className="absolute w-full h-full object-cover" />
+        <span className="font-bold text-xl z-10 text-white ml-14 mb-12">this is Homepage boyy</span>
+      </div>
+    </MainLayout>
+  )
+}
 
-export default homepage;
+export default homepage
