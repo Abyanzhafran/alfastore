be823e4a80ca552d61565c982c8032d6e76898a2
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-09-13 21:35:29 +0700

    feat(ui): :lipstick: add productc card, update homepage, add asset

---
+++ components/ProductCard.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+// import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/solid';
+import { HTMLAttributes } from 'react';
+
+const ProductCard = ({ ...props }: HTMLAttributes<HTMLElement>) => (
+  <div
+    {...props}
+  >
+    {/* <Image
+      src="/alfa.png"
+      className="object-fill"
+      width="232"
+      height="240"
+    /> */}
+    <img
+      src="/alfa.png"
+      className="w-56 h-56 object-cover"
+    />
+    <div className="flex flex-col items-center justify-center bg-white">
+      <div className="my-6">
+        <span className="text-base leading-6 font-medium capitalize">
+          meja ergonomis
+        </span>
+        <div className="flex items-center justify-center gap-1.5">
+          {[...Array(5)].map((i) => (
+            <StarIcon
+              className="w-6 h-6 text-yellow-400"
+              values={i}
+            />
+          ))}
+          <span className="text-xs leading-4 font-normal">
+            (167)
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default ProductCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/alt-text */
 import type { NextPage } from 'next';
 import MainLayout from 'components/MainLayout';
+import { ChevronRightIcon, ChevronLeftIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
+import ProductCard from 'components/ProductCard';
 
 const homepage: NextPage = () => (
   <MainLayout className="">
-    <div className="relative w-full h-screen flex">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
+    <header className="relative w-full h-screen flex">
       <img
         src="/alfa.png"
         className="w-full h-full object-cover"
@@ -16,11 +19,73 @@ const homepage: NextPage = () => (
         style={{ background: 'linear-gradient(180deg, rgba(196, 196, 196, 0) 20.51%, rgba(39, 39, 42, 0.7) 64.84%, #171717 100%)' }}
       />
 
-      <div className="absolute bottom-12 left-14">
-        <span className="text-xl leading-7 font-bold z-10 text-white uppercase">General Supplier</span>
-        <hr className="text-white mb-2 h-px w-20" />
+      <div className="absolute bottom-12 left-14 flex flex-col">
+        <span className="text-xl leading-7 font-bold z-10 text-white uppercase mb-3">General Supplier</span>
+        <hr className="text-white border-2 w-20 mb-4" />
+        <span className="text-base leading-6 font-normal text-white mb-6">
+          Melayani berbagai kebutuhan pasokan barang untuk kebutuhan kantor anda maupun kebutuhan lainnya
+        </span>
+        <div className="flex gap-5 items-center">
+          <button className="bg-white hover:bg-gray-500 px-1.5 py-3 text-sm leading-5 font-medium">
+            Kontak Kami
+          </button>
+          <button className="text-sm leading-5 font-medium text-white flex">
+            Lihat produk
+            <ArrowSmRightIcon className="w-6 h-6 text-white" />
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="absolute bottom-12 right-14 flex">
+        <button>
+          <ChevronLeftIcon className="w-6 h-6 text-gray-500" />
+        </button>
+        <button>
+          <ChevronRightIcon className="w-6 h-6 text-white" />
+        </button>
+      </div>
+    </header>
+
+    <main className="w-full">
+      <section className="flex flex-col items-center justify-center">
+        <span className="text-lg leading-7 font-bold flex flex-col items-center mt-8">
+          klien kami
+          <hr className="border-black border-2 w-20 mt-3" />
+        </span>
+      </section>
+      <section className="flex flex-col items-center justify-center my-20 bg-gray-50">
+        <div className="mx-6 mt-8">
+          <span className="text-lg leading-7 font-bold flex flex-col items-center">
+            Barang Terlaris
+            <hr className="border-black border-2 w-20 mt-3" />
+          </span>
+          <div className="flex flex-wrap justify-center gap-4 my-11">
+            {[...Array(8)].map((i) => (
+              <ProductCard
+                key={i}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center">
+        <span className="text-lg leading-7 font-bold flex flex-col items-center my-8">
+          Mitra Kami
+          <hr className="border-black border-2 w-20 mt-3" />
+        </span>
+      </section>
+      <section className="flex flex-col items-center justify-center">
+        <span className="text-lg leading-7 font-bold flex flex-col items-center my-8">
+          Temukan Kami
+          <hr className="border-black border-2 w-20 mt-3" />
+          <div className="mt-11">
+            <img
+              src="/map.png"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </span>
+      </section>
+    </main>
   </MainLayout>
 );
 
